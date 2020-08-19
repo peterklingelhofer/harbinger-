5ff48c31e6987e6ed4ca4860ffa91bf83cdc5d69
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-19 10:48:28 -0500

    Temporary changes

--- client/src/homepage.jsx
+++ client/src/homepage.jsx
@@ -8,6 +8,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import { useForm } from 'react-hook-form';
 import Search from './search.jsx';
 import ReviewList from './ReviewList.jsx';
+import PhotoUpload from './PhotoUpload.jsx';
 
 function HomePage() {
   const [user, setUser] = useState([]);
@@ -100,6 +101,7 @@ function HomePage() {
         </Background>
       </div>
       <Search />
+      <PhotoUpload />
       <Background style={{ color: "white", marginLeft: "600px" }}>
         <h2>Top Best Reviews</h2>
       </Background>
