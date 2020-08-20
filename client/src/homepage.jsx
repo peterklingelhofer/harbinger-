354a61962e193bda14ff01eae08c008a15f0c0ba
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-20 11:45:03 -0500

    homepage.jsx moved

--- client/src/homepage.jsx
+++ client/src/homepage.jsx
@@ -1,36 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { styled } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Toolbar from '@material-ui/core/Toolbar';
+// import { styled } from '@material-ui/core';
+// import Button from '@material-ui/core/Button';
+// import Toolbar from '@material-ui/core/Toolbar';
 import { useForm } from 'react-hook-form';
 import Search from './search.jsx';
 import ReviewList from './ReviewList.jsx';
+import { MyButton, Background } from '../styles';
 
 function HomePage() {
   const [user, setUser] = useState([]);
   const { handleSubmit } = useForm();
-  // | Material UI style methods go here:
-  //  \ be welcome to expand on the palethra of styles and add them to methods.
-  //    \ use method as a tag to render the custom style.
-  const MyButton = styled(Button)({
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 20,
-    padding: '0 20px',
-  });
-
-  const Background = styled(Toolbar)({
-    background: 'linear-gradient(45deg, #FE6242 30%, #FF2445 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'red',
-  });
 
   useEffect(() => {
     axios.get('/good').then(({ data }) => {
